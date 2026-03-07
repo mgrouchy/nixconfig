b799b63a617e9c38f51a97e20573fb3196eaef1a
Mike Grouchy <mgrouchy@gmail.com>
2026-03-07 14:28:17 -0500

    shared: extract neovim config

--- modules/shared/home-manager.nix
+++ modules/shared/home-manager.nix
@@ -7,124 +7,8 @@
 
 let
   name = "Mike Grouchy";
-  user = "mgrouchy";
   email = "mgrouchy@gmail.com";
-  vimPlugins = with pkgs.vimPlugins; [
-    vim-airline
-    vim-airline-themes
-    vim-startify
-    vim-tmux-navigator
-  ];
-  sharedVimConfig = ''
-    "" General
-    set number
-    set history=1000
-    set nocompatible
-    set modelines=0
-    set encoding=utf-8
-    set scrolloff=3
-    set showmode
-    set showcmd
-    set hidden
-    set wildmenu
-    set wildmode=list:longest
-    set cursorline
-    set ttyfast
-    set nowrap
-    set ruler
-    set backspace=indent,eol,start
-    set laststatus=2
-    if has('nvim')
-      set clipboard=unnamedplus
-    else
-      set clipboard=autoselect
-    endif
-
-    " Dir stuff
-    set nobackup
-    set nowritebackup
-    set noswapfile
-    set backupdir=~/.config/vim/backups
-    set directory=~/.config/vim/swap
-
-    " Relative line numbers for easy movement
-    set relativenumber
-    set rnu
-
-    "" Whitespace rules
-    set tabstop=8
-    set shiftwidth=2
-    set softtabstop=2
-    set expandtab
-
-    "" Searching
-    set ignorecase
-    set incsearch
-    set gdefault
-
-    "" Statusbar
-    set nocompatible " Disable vi-compatibility
-    set laststatus=2 " Always show the statusline
-    let g:airline_theme='bubblegum'
-    let g:airline_powerline_fonts = 1
-
-    "" Local keys and such
-    let mapleader=","
-    let maplocalleader=" "
-
-    "" Change cursor on mode
-    :autocmd InsertEnter * set cul
-    :autocmd InsertLeave * set nocul
-
-    "" File-type highlighting and configuration
-    syntax on
-    filetype on
-    filetype plugin on
-    filetype indent on
-
-    "" Paste from clipboard
-    nnoremap <Leader>, "+gP
-
-    "" Copy from clipboard
-    xnoremap <Leader>. "+y
-
-    "" Move cursor by display lines when wrapping
-    nnoremap j gj
-    nnoremap k gk
-
-    "" Map leader-q to quit out of window
-    nnoremap <leader>q :q<cr>
-
-    "" Move around split
-    nnoremap <C-h> <C-w>h
-    nnoremap <C-j> <C-w>j
-    nnoremap <C-k> <C-w>k
-    nnoremap <C-l> <C-w>l
-
-    "" Easier to yank entire line
-    nnoremap Y y$
-
-    "" Move buffers
-    nnoremap <tab> :bnext<cr>
-    nnoremap <S-tab> :bprev<cr>
-
-    "" Like a boss, sudo AFTER opening the file to write
-    cmap w!! w !sudo tee % >/dev/null
-
-    let g:startify_lists = [
-      \ { 'type': 'dir',       'header': ['   Current Directory '. getcwd()] },
-      \ { 'type': 'sessions',  'header': ['   Sessions']       },
-      \ { 'type': 'bookmarks', 'header': ['   Bookmarks']      }
-      \ ]
-
-    let g:startify_bookmarks = [
-      \ '~/Projects',
-      \ '~/Documents',
-      \ ]
-
-    let g:airline_theme='bubblegum'
-    let g:airline_powerline_fonts = 1
-  '';
+  neovimConfig = import ./config/neovim.nix { inherit pkgs; };
 in
 {
   # Shared shell configuration
@@ -193,7 +77,7 @@ in
     shellAliases = {
       ports = ''sudo lsof -i -P | grep -i "listen"'';
       ips = ''ifconfig -a | grep -o 'inet6\? \(\([0-9]\+\.[0-9]\+\.[0-9]\+\.[0-9]\+\)\|[a-fA-F0-9:]\+\)' | sed -e 's/inet6* //'';
-      code = ''/run/current-system/sw/bin/Cursor $1'';
+      code = "/run/current-system/sw/bin/Cursor $1";
       ls = "ls --color=auto";
       cat = "bat";
       za = ''zellij attach -c "''${USER}@$(hostname)"'';
@@ -290,8 +174,7 @@ in
     };
     settings = {
       user = {
-        name = name;
-        email = email;
+        inherit name email;
       };
 
       alias = {
@@ -360,11 +243,10 @@ in
 
   neovim = {
     enable = true;
-    plugins = vimPlugins;
-    extraConfig = sharedVimConfig;
     viAlias = true;
     vimAlias = true;
-  };
+  }
+  // neovimConfig;
 
   direnv = {
     enable = true;
